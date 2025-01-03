247e69fb52ac5673e496a65b9d81b1519ae26c68
kochamkotkiic <01197869@pw.edu.pl>
2025-01-03 21:41:38 +0100

    main zrobiony troche

--- interfejs.c
+++ interfejs.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include "interfejs.h"
 
+
 void wypisz_plansze(char **plansza, int wiersze, int kolumny) {
     for (int i = 0; i < wiersze; i++) {
         for (int j = 0; j < kolumny; j++) {
             if (plansza[i][j] == 'X') {
                 printf("? ");  // zakryte pole
-            } 
-            else if (plansza[i][j] == 'F') {
+            } else if (plansza[i][j] == 'F') {
                 printf("F ");  // flaga
-            } 
-            else {
-                printf("%c ", plansza[i][j]);  // odkryte pole z liczbą lub miną
+            } else if (plansza[i][j] == '*') {
+                printf("* ");  // mina
+            } else {
+                printf("%c ", plansza[i][j]);  // odkryte pole z liczbą
             }
         }
         printf("\n");
@@ -19,5 +20,6 @@ void wypisz_plansze(char **plansza, int wiersze, int kolumny) {
 }
 
 void zaktualizuj_plansze(char **plansza, int wiersze, int kolumny, int x, int y, char znak) {
-    plansza[x][y] = znak; //aktualizacja o dany znak
-    wypisz_plansze(plansza, wiersze, kolumny);  /
+    plansza[x][y] = znak; //aktualizacja o dane pole
+    wypisz_plansze(plansza, wiersze, kolumny);  
+}
--- main.c
+++ main.c
@@ -1,3 +1,96 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 #include "plansza.h"
+#include "interfejs.h"
 
+void uruchom_gre(int wiersze, int kolumny, int liczba_min);
+void uruchom_z_pliku(const char *sciezka);
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("brak argumentu. Użyj -f <ścieżka do pliku> lub uruchom grę w trybie normalnym.\n");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "-f") == 0 && argc == 3) {
+        // uruchomienie gry z pliku
+        uruchom_z_pliku(argv[2]);
+    } else {
+        // normalne uruchomienie gry
+        int poziom_trudnosci;
+        printf("wybierz poziom trudności (1 - łatwy, 2 - średni, 3 - trudny): ");
+        scanf("%d", &poziom_trudnosci);
+
+        int wiersze, kolumny, liczba_min;
+
+        switch (poziom_trudnosci) {
+            case 1:
+                wiersze = 9; kolumny = 9; liczba_min = 10; break;
+            case 2:
+                wiersze = 16; kolumny = 16; liczba_min = 40; break;
+            case 3:
+                wiersze = 16; kolumny = 30; liczba_min = 99; break;
+            default:
+                printf("nieprawidłowy poziom trudności.\n");
+                return 1;
+        }
+
+        uruchom_gre(wiersze, kolumny, liczba_min);
+    }
+
+    return 0;
+}
+
+void uruchom_gre(int wiersze, int kolumny, int liczba_min) {
+    char **plansza;
+    tworzenie_planszy(&plansza, wiersze, kolumny);
+    losowanie_min(plansza, wiersze, kolumny, liczba_min);
+    obliczanie_sasiednich_min(plansza, wiersze, kolumny);
+    wypisz_plansze(plansza, wiersze, kolumny);
+
+    // Dalsza logika gry (np. wczytywanie ruchów gracza i aktualizacja planszy)
+    // Na przykład możesz tu wprowadzić pętlę do przyjmowania komend
+}
+
+void uruchom_z_pliku(const char *sciezka) {
+    FILE *plik = fopen(sciezka, "r");
+    if (!plik) {
+        printf("nie udało się otworzyć pliku.\n");
+        return;
+    }
+
+    int wiersze, kolumny;
+    fscanf(plik, "%d %d\n", &wiersze, &kolumny);
+
+    char **plansza;
+    tworzenie_planszy(&plansza, wiersze, kolumny);
+
+    // odczytujemy planszę z pliku
+    for (int i = 0; i < wiersze; i++) {
+        for (int j = 0; j < kolumny; j++) {
+            fscanf(plik, " %c", &plansza[i][j]);
+        }
+    }
+
+    // odczytujemy ruchy i aktualizujemy planszę
+    char komenda;
+    int x, y;
+    while (fscanf(plik, " %c %d %d", &komenda, &x, &y) == 3) {
+        if (komenda == 'f') {
+            plansza[x][y] = 'F';  // flaga
+        } 
+        else if (komenda == 'r') {
+            if (plansza[x][y] == '*') {
+                printf("trafiłeś na minę! Koniec gry.\n");
+                break;
+            }
+            else {
+                wypisz_plansze(plansza, wiersze, kolumny);  // zaktualizowana plansza
+            }
+        }
+    }
+
+    fclose(plik);
+}
